0edb054d6ccaab138b9e7dab63a230e025737230
Faysal SANDA <faysalgluma@gmail.com>
2021-11-02 09:07:55 +0100

    Initialise repository

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+~
+Napp/src/main/java/com/goupec/savearraylistsharepreferences/ExampleAdapter.java,b/d/bd93338988025576ae807a4ab93c76a15df96843
+{
+Kapp/src/main/java/com/goupec/savearraylistsharepreferences/ExampleItem.java,5/a/5ad85ac6c7be5375408ff2cfe7cbbafd3634c54e
+|
+Lapp/src/main/java/com/goupec/savearraylistsharepreferences/MainActivity.java,4/a/4a3ce4dcdc05d3372844407342edba73ab852f4e
---
+++ app/src/main/java/com/goupec/savearraylistsharepreferences/MainActivity.java
@@ -0,0 +1,93 @@
+package com.goupec.savearraylistsharepreferences;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<ExampleItem> mExampleList;
+    private RecyclerView mRecyclerView;
+    private ExampleAdapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadData();
+        buildRecyclerView();
+        setInsertButton();
+
+        Button buttonSave = findViewById(R.id.button_save);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveData();
+            }
+        });
+    }
+
+    private void saveData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Gson gson = new Gson();
+        String json = gson.toJson(mExampleList);
+        editor.putString("taskList", json);
+        editor.apply();
+
+        Toast.makeText(this, "Saved Array List to Shared preferences. ", Toast.LENGTH_SHORT).show();
+    }
+
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPref", MODE_PRIVATE);
+        Gson gson = new Gson();
+        String json = sharedPreferences.getString("taskList", null);
+        Type type = new TypeToken<ArrayList<ExampleItem>>() {}.getType();
+        mExampleList = gson.fromJson(json, type);
+
+        if (mExampleList == null) {
+            mExampleList = new ArrayList<>();
+        }
+    }
+
+    private void buildRecyclerView() {
+        mRecyclerView = findViewById(R.id.recyclerview);
+        mRecyclerView.setHasFixedSize(true);
+        mLayoutManager = new LinearLayoutManager(this);
+        mAdapter = new ExampleAdapter(mExampleList);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    private void setInsertButton() {
+        Button buttonInsert = findViewById(R.id.button_insert);
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText line1 = findViewById(R.id.edittext_line_1);
+                EditText line2 = findViewById(R.id.edittext_line_2);
+                insertItem(line1.getText().toString(), line2.getText().toString());
+            }
+        });
+    }
+
+    private void insertItem(String line1, String line2) {
+        mExampleList.add(new ExampleItem(line1, line2));
+        mAdapter.notifyItemInserted(mExampleList.size());
+    }
+}
